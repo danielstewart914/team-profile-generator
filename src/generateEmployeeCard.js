f1c9252b6be0a82ca7eb55d3cf2056b956c6652b
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-08-08 12:38:11 -0700

    Finished generateHTML function

--- src/generateEmployeeCard.js
+++ src/generateEmployeeCard.js
@@ -1,19 +1,20 @@
-const Manager = require( '../lib/Manager' );
-const Engineer = require( '../lib/Engineer' );
-const Intern = require( '../lib/Intern' );
-
+// return last line of info based upon employee role
 const getOtherInfo = ( employee ) => {
 
     const role = employee.getRole();
 
+    // if employee is a manager return office number
     if ( role === 'Manager' ) 
         return `Office Number: ${ employee.officeNumber }`;
+    // if employee is a engineer return github profile link
     if ( role === 'Engineer' ) 
         return `GitHub: <a href="https://www.github.com/${ employee.gitHub }">${ employee.gitHub }</a>`;
+    // if employee is an intern return school
     if ( role === 'Intern' )
         return `School: ${ employee.school }`;
 }
 
+// return icons base upon role
 const getIcon = ( role ) => {
 
     if ( role  === 'Manager' ) 
@@ -33,6 +34,7 @@ const getIcon = ( role ) => {
         };
 }
 
+// generate a card for displaying employee info
 const generateEmployeeCard = ( employee ) =>{
 
     const { id, email, name } = employee;
@@ -40,27 +42,22 @@ const generateEmployeeCard = ( employee ) =>{
     const icon = getIcon( employee.getRole() );
 
     const cardTemplate = `
-<div class="card col-12 col-md-6 col-lg-4 col-xl-3 p-0 m-2 shadow">
-    <div class="card-header bg-primary text-white text-center">
-        <h2>${ name }</h2>
-        <p class="role"><img class="invert icon" src="${ icon.path }" alt="${ icon.alt }"> ${ employee.getRole() }</p>
-    </div>
-    <div class="card-body d-flex justify-content-center align-items-center">
-        <ul class="list-group col-12">
-            <li class="list-group-item">ID: ${ id }</li>
-            <li class="list-group-item">Email: <a href="mailto:${ email }">${email }</a></li>
-            <li class="list-group-item">${ getOtherInfo( employee ) }</li>
-        </ul>
-    </div>
-</div>`;
+            <div class="card col-12 col-md-6 col-lg-4 col-xl-3 p-0 m-2 shadow">
+                <div class="card-header bg-primary text-white text-center">
+                    <h2>${ name }</h2>
+                    <p class="role"><img class="invert icon" src="${ icon.path }" alt="${ icon.alt }"> ${ employee.getRole() }</p>
+                </div>
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <ul class="list-group col-12">
+                        <li class="list-group-item">ID: ${ id }</li>
+                        <li class="list-group-item">Email: <a href="mailto:${ email }">${email }</a></li>
+                        <li class="list-group-item">${ getOtherInfo( employee ) }</li>
+                    </ul>
+                </div>
+            </div>`;
 
 return cardTemplate;
 
 }
-
-console.log( generateEmployeeCard( new Manager( 'Daniel', 1, 'danielstewart914@outlook.com', 1 ) ) );
-console.log( generateEmployeeCard( new Engineer( 'Daniel', 4, 'danielstewart914@outlook.com', 'danielstewart914' ) ) );
-console.log( generateEmployeeCard( new Intern( 'Daniel', 42, 'danielstewart914@outlook.com', 'University of Washington' ) ) );
-
 
 module.exports = generateEmployeeCard;
---
+++ src/generateHTML.js
@@ -0,0 +1,54 @@
+const Manager = require( '../lib/Manager' );
+const Engineer = require( '../lib/Engineer' );
+const Intern = require( '../lib/Intern' );
+const generateEmployeeCard = require( './generateEmployeeCard' );
+
+// generate string of HTML cards from array of Employees
+const generateEmployeeList = ( employees ) => {
+
+    let htmlString = '';
+
+    for ( const employee of employees ) {
+        htmlString += generateEmployeeCard( employee );
+    }
+
+    return htmlString;
+}
+
+// generate full HTML page for displaying team info
+const generateHTML = ( employees ) => {
+
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>My Team</title>
+</head>
+<body>
+    <header class="bg-danger text-light p-4 text-center">
+        <h1>My Team</h1>
+    </header>
+    <main class="container pt-5">
+        <div class="row justify-content-center p-4">${ generateEmployeeList( employees ) }
+        </div>
+    </main>
+</body>
+</html>`;
+
+}
+
+const employees = [
+    new Manager( 'Daniel', 1, 'danielstewart914@outlook.com', 1 ),
+    new Engineer( 'Daniel', 2, 'danielstewart914@outlook.com', 'danielstewart914' ),
+    new Engineer( 'Bob', 3, 'bobby@dev.com', 'bob123' ),
+    new Intern( 'Anthony', 4, 'tony@email.com', 'University of Washington' ) 
+]
+
+console.log( generateHTML( employees ) );
+
+module.exports = generateHTML;
